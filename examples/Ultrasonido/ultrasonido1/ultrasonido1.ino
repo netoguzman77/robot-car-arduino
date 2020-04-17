105d4a235de0ec559c84d834ee7e359cff5c8380
evelyn-rivera <54190647+evelyn-rivera@users.noreply.github.com>
2020-04-16 23:58:10 -0600

    inicio robo car

---
+++ examples/Ultrasonido/ultrasonido1/ultrasonido1.ino
@@ -0,0 +1,18 @@
+#include <robot-car-arduino.h>
+byte pinEcho=9;
+byte pinTrigger=10;
+
+//Ultrasonido sensor1(&pinTrigger,&pinEcho);//declaramos y construimos
+Ultrasonido sensor2; //se esta invocando al constructor por defecto
+
+void setup(){
+    Serial.begin(9600);
+    sensor2= Ultrasonido(&pinTrigger , &pinEcho);//se invoca al constructor
+
+}
+ void loop(){
+     //Serial.println(sensor1.medirCM());//llamado a sensor1
+     Serial.println(sensor2.medirCM());//llamado a sensor 2
+     delay(5000);
+ }
+ //agregar
