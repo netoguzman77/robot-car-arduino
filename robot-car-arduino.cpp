9c3b8732da0465d6361cac1c9b142adbfef8f440
monzondev <alexandermm2011@gmail.com>
2020-04-23 14:54:20 -0600

    Agrengando funciones en el archivo fuente de la libreria

--- robot-car-arduino.cpp
+++ robot-car-arduino.cpp
@@ -2,12 +2,40 @@
 MICROPROGRAMACION 2020
 */
 
-#include "arduino.h"
+#include <Arduino.h>
 #include "robot-car-arduino.h"
 /* **************************************
 **      CLASE ULTRASONIDO
 ************************************** */
 
+//constructor sin parametros
+Ultrasonido::Ultrasonido(){}
+
+//Constructor con parametros
+Ultrasonido::Ultrasonido(byte *_pinTrigger, byte *_pinEcho){
+
+    pinTrigger=_pinTrigger;
+
+    pinEcho=_pinEcho;
+
+    pinMode(*pinTrigger, OUTPUT);
+
+    pinMode(*pinEcho, INPUT);
+
+}
+
+//Hace la medicion de distancia devuelve cms de distancia hasta 350cm maximo
+int Ultrasonido::medirCM(){
+    digitalWrite(*pinTrigger, LOW);
+    delayMicroseconds(4);
+    digitalWrite(*pinTrigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(*pinTrigger, LOW);
+    distancia=pulseIn(*pinEcho,HIGH)/56.5812;
+    distancia=constrain(distancia,0,350);
+    return(int) distancia;
+}
+ 
 
 
 
