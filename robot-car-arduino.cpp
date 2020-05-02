ad1d7add9953cf4e8d999ea5d3aa41114ac8b80d
Hector009 <MG17009@ues.edu.sv>
2020-05-01 18:08:48 -0600

    Actualizacion
    MG17009

--- robot-car-arduino.cpp
+++ robot-car-arduino.cpp
@@ -7,11 +7,24 @@ MICROPROGRAMACION 2020
 /* **************************************
 **      CLASE ULTRASONIDO
 ************************************** */
+//constructor parametrizado
+Ultrasonido::Ultrasonido(byte *_pinTrigger, byte *_pinEcho){
+  pinTrigger=_pinTrigger;
+  pinEcho=_pinEcho;
+  pinMode(*pinTrigger, OUTPUT);
+  pinMode(*pinEcho, INPUT);
+}
+Ultrasonido::Ultrasonido(){}
 
-
-
-
-
+int Ultrasonido::medirCM(){
+    digitalWrite(*pinTrigger,LOW);
+    delayMicroseconds(4);
+    digitalWrite(*pinTrigger,HIGH);
+    delayMicroseconds(4);
+    digitalWrite(*pinTrigger,LOW);
+    distancia=pulseIn(*pinEcho,HIGH)/56.5812;
+    distancia=constrain(distancia,0,350);
+    return (int) distancia;
 
 
 /*  **************************************************************
--- robot-car-arduino.h
+++ robot-car-arduino.h
@@ -7,6 +7,9 @@ ciclo I-2019   DEFINA LAS SIGUIENTES CLASES
 + miembro : tipodedatos
 */
 
+#ifndef robot-car-arduino_h
+#define robot-car-arduino_h
+#include "arduino.h"
 
 /*************************    
 	Clase Ultrasonido
@@ -18,6 +21,20 @@ ciclo I-2019   DEFINA LAS SIGUIENTES CLASES
 + medirCM() : int
 + Ultrasonido(byte*,byte*)
 ****************************/
+class Ultrasonido{
+    private:
+	  long distancia;
+	public:
+	   byte *pinEcho;
+	   byte *pinTrigger;
+
+      
+       Ultrasonido(byte *_pinTrigger, byte *_pinEcho);
+       Ultrasonido();
+
+
+       int medirCM();
+};
 
 
 
@@ -66,4 +83,53 @@ ciclo I-2019   DEFINA LAS SIGUIENTES CLASES
 	+ girar(int grados): void
 
 *******************************************************************************/
+
+//********************C L A S E  C A R R O **********************
+class Carro{
+	public:
+	byte encoderI=2;   //pin del encoder de la llanta izquierda
+	byte encoderD=3;   //pin del encoder de la llanta derecha
+	byte adelanteI=4;  //pin avance adelante llanta izquierda
+	byte atrasI=5;     //pin de reversa llanta izquierda
+	byte potenciaI=6;  //pin de potencia llanta izquierda pin PWM
+	byte adelanteD=7;  //adelante llanta derecha
+	byte atrasD=8;     //atras llanta derecha
+	byte potenciaD=9;  //potencia llanta derecha pin PWM
+	byte sensortrigerC=10; //sensor ultrasonido central trigger
+	byte sensorechoC=11; //sensor ultrasonido central echo
+	byte sensortrigerI=12; //sensor ultrasonido izquierda trigger
+	byte sensorechoI=13; //sensor ultrasonido izquierda echo
+	byte sensortrigerD=14; //sensor ultrasonido derecha trigger
+	byte sensorechoD=15; //sensor ultrasonido derecho echo
+	byte potenciaMAX=200;
+
+	Ultrasonido UltraC; //
+	Ultrasonido UltraI; //
+	Ultrasonido UltraD;
+
+
+	//DEFINIENDO LAS INTERRUPCIONES--USAN MIEMBROS ESTATICOS
+	static volatile unsigned int contaI; //contador de las vueltas de la llanta izquierda
+	static volatile unsigned int contaD; //contador de las vueltas de la llanta derecha
+	//CONTADORES DE RUEDAS IZQUIERDA Y DERECHA
+	static void contarRuedaI(); //metodo a registrar con la interrupcion en constructor
+	static void contarRuedaD();
+
+	//Constructor -- configura variables, sensores e interrupciones
+	Carro();
+
+	//acciona el motor,recibe como parametro la accion en la llanta izquierda y la llanta derecha
+	//el valor de cada llanta si es positivo movera la llanta hacia adelante
+	//si es negativo movera la llanta hacia atras
+	//el numero representara la potencia aplicada a cada llanta
+	//los valores pueden ir de -10 hasta 10
+	void mover(int Izquierda, int Derecha);
+
+	//gira la cantidad de grados especifica: - a la izquierda, + derecha
+	//usa los contadores de vuelta para la presicion al girar
+	//tu decides si usas dos llantas para girar o solo una
+	void girar(int grados);
+};
+
+#endif
 
