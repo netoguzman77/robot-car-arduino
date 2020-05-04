b7ed7479ade45b0efe2287e283ad1415813874e8
Esperanza Duenas <esperanza.duenas@unplug.studio>
2020-05-03 20:45:53 -0600

    General: Clase Carro definida

--- robot-car-arduino.h
+++ robot-car-arduino.h
@@ -38,9 +38,6 @@ class Ultrasonido{
 	// por tanto el valor maximo es 350 cm 
 	int medirCM();
 };
-#endif
-
-
 
 /*******************************************************************************
 						Clase Carro
@@ -87,4 +84,47 @@ class Ultrasonido{
 	+ girar(int grados): void
 
 *******************************************************************************/
+class Carro{
+	public:
+		byte encoderI  = 2; //pin del encoder de la llanta izquierda
+		byte encoderD  = 3; // pin del encoder de la llanta derecha
+		byte adelanteI = 4; // pin avance adelante llanta Izquierdabyte 
+		byte atrasI    = 5;    // pin de reversa llanta izquierda
+		byte potenciaI = 6; // pin de potencia llanta Izquierda pin PWM
+		byte adelanteD = 7; // Adelante llanta Derechabyte 
+		byte atrasD    = 8;  // atras llanta Derecha
+		byte potenciaD = 9;  // potencia llanta Derecha pin PWM
+		byte sensortrigerC = 10; //Sensor Ultrasonido Central Trigger
+		byte sennsorechoC  = 11; //Sensor Ultrasonido Central Echo
+		byte sensortrigerI = 12; //Sensor Ultrasonido Izquierda Trigger
+		byte sensorechoI   = 13; //Sensor Ultrasonido Izquierda Echo
+		byte sensortrigerD = 14; //Sensor Ultrasonido Derecha Trigger
+		byte sensorechoD   = 15; //Sensor Ultrasonido Derecha Echo
+		byte potenciaMAX   = 200;  //limite regulador de la potencia maxima
+		
+		Ultrasonido UltraC; // Se declara como PUNTEROS
+		Ultrasonido UltraI; // en el cpp se va a instanciar y a referir
+		Ultrasonido UltraD;
 
+		//DEFINIENDO LAS INTERRUPCIONES--USAN MIEMBROS ESTATICOS
+  		static volatile unsigned int contaI; //contador de las vueltas de la llanta Izquierda
+  		static volatile unsigned int contaD; //contador de las vueltas de la llanta Derecha
+		//CONTADORES DE RUEDAS IZQUIERDA Y DERECHA
+		static void contarRuedaI(); //Metodo para registrar con la interrupcion en Constructor
+		static void contarRuedaD();
+
+		//Constructor -- configura variables, sensores e interrupciones
+		Carro();
+		// Acciona el motor, recibe como parametro la accion en la llanta izquierda y la llanta derecha
+		// el valor de cada llanta si es positivo movera la llanta hacia adelante
+		// si es negativo movera la llanta hacia atras
+		// el numero representara la potencia aplicada a cada llanta
+		// los valores pueden ir de -10 hasta 10
+		void mover(int Izquierda, int Derecha);
+		//Gira la cantidad de grados especifica: - a la izquierda, + derecha
+		//usa los contadores de vuelta para la precision al girar
+		//tu decides si usas dos llantas para girar o solo una
+		void girar(int grados);
+	};
+
+#endif
