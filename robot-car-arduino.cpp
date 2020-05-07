92b4387ca860a34f99419a5b47859d07cff3b510
brahhms <abrahamatteo@gmail.com>
2020-05-07 11:13:04 -0600

    se modifico carro cpp

--- robot-car-arduino.cpp
+++ robot-car-arduino.cpp
@@ -9,28 +9,27 @@ MICROPROGRAMACION 2020
 **      CLASE ULTRASONIDO
 ************************************** */
 //vacio
-Ultrasonido::Ultrasonido(){}
+Ultrasonido::Ultrasonido() {}
 //parametrizado
-Ultrasonido::Ultrasonido(byte *_pinTrigger, byte *_pinEcho){
-    pinTrigger=_pinTrigger;
-    pinEcho=_pinEcho;
+Ultrasonido::Ultrasonido(byte *_pinTrigger, byte *_pinEcho)
+{
+    pinTrigger = _pinTrigger;
+    pinEcho = _pinEcho;
     pinMode(*pinTrigger, OUTPUT);
     pinMode(*pinEcho, INPUT);
 }
 
-
-
 //hace la medicion de distancia devuelve cms de distancia hasta 350cm maxi
-int Ultrasonido::medirCM(){
-    digitalWrite(*pinTrigger,LOW);
+int Ultrasonido::medirCM()
+{
+    digitalWrite(*pinTrigger, LOW);
     delayMicroseconds(4);
-    digitalWrite(*pinTrigger,HIGH);
+    digitalWrite(*pinTrigger, HIGH);
     delayMicroseconds(10);
-    digitalWrite(*pinTrigger,LOW);
-    distancia=pulseIn(*pinEcho,HIGH)/56.5812;
-    distancia=constrain(distancia,0,350);
-    return (int) distancia;
-
+    digitalWrite(*pinTrigger, LOW);
+    distancia = pulseIn(*pinEcho, HIGH) / 56.5812;
+    distancia = constrain(distancia, 0, 350);
+    return (int)distancia;
 }
 
 /*  **************************************************************
@@ -45,10 +44,20 @@ Carro::Carro()
 {
     //configurando los pinMode del la potencia del carro
     //encoder, adelante, atras, potencia
+    pinMode(encoderI, INPUT);
+    pinMode(encoderD, INPUT);
+    pinMode(adelanteI, OUTPUT);
+    pinMode(atrasI, OUTPUT);
+    pinMode(potenciaI, OUTPUT);
+    pinMode(adelanteD, OUTPUT);
+    pinMode(atrasD, OUTPUT);
+    pinMode(potenciaD, OUTPUT);
 
     //instanciando los sensores ultrasonido y refiriendo
     // invoco constructor parametrizado, new devuelve un puntero
-    //UltraD->medir(); //para acceder a sus miembros uso -> por ser puntero
+    UltraC = Ultrasonido(&sensortrigerC, &sensorechoC);
+    UltraI = Ultrasonido(&sensortrigerI, &sensorechoI);
+    UltraD = Ultrasonido(&sensortrigerD, &sensorechoD);
 
     //Agregamos este codigo para registrar las interrupciones
     //RISING indica que se disparara la interrupcion cuando el pin cambie de 0 a 1
@@ -76,9 +85,84 @@ void Carro::contarRuedaD()
 }
 
 //metodo para mover el carro + adelante, - atras, potencia abs
+// -10 y +10
+void Carro::mover(int izquierda, int derecha)
+{
+    izquierda = constrain(izquierda, -10, 10);
+    derecha = constrain(derecha, -10, 10);
+
+    //Rueda Izquierda
+    if (izquierda < 0)
+    {
+        digitalWrite(adelanteI, LOW);
+        digitalWrite(atrasI, HIGH);
+    }
+    else if (izquierda > 0)
+    {
+        digitalWrite(adelanteI, HIGH);
+        digitalWrite(atrasI, LOW);
+    }
+    else
+    {
+        digitalWrite(adelanteI, LOW);
+        digitalWrite(atrasI, LOW);
+    }
+
+    //Rueda Derecha
+    if (derecha < 0)
+    {
+        digitalWrite(adelanteD, LOW);
+        digitalWrite(atrasD, HIGH);
+    }
+    else if (derecha > 0)
+    {
+        digitalWrite(adelanteD, HIGH);
+        digitalWrite(atrasD, LOW);
+    }
+    else
+    {
+        digitalWrite(adelanteD, LOW);
+        digitalWrite(atrasD, LOW);
+    }
+
+    //potencia indicada en pines PWM
+    //AJUSTANDO LA POTENCIA
+
+    derecha = abs(derecha);
+    izquierda = abs(izquierda);
+
+    izquierda = map(izquierda, 0, 10, 0, potenciaMAX);
+    derecha = map(derecha, 0, 10, 0, potenciaMAX);
+    analogWrite(potenciaD, derrecha);
+    analogWrite(potenciaI, izquierda);
+}
 
 //Gira la cantidad de grados especifica: - a la izquierda, + derecha
 //usa los contadores de vuelta para la precision al girar
 //tu decides si usas dos llantas para girar o solo una
 //Si tienes contadores de rueda los usaras para hacer el giro
 //si no tienes contadores haras un aproximado con delay(tiempo)
+void Carro::girar(int grados)
+{
+    float medioGiro = 40;
+    int parar = grados * (medioGiro / 180);
+    parar = abs(parar);
+    mover(0, 0);
+    delay(100) : contaI = 0;
+    contaD = 0;
+    if (grados > 0)
+    {
+        mover(10, -10); //girando moviendo las dos llantas
+    }
+    else if (grados < 0)
+    {
+        mover(-10, 10);
+    }
+
+    while (contaI + contaD < parar)
+    {
+        /* code */
+    }
+    mover(0, 0);
+    delay(100);
+}
