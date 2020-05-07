213fe325139014e145e0b3759b9931940eb580f2
Enrique Moreno <flakitoquique@gmail.com>
2020-05-07 13:24:35 -0600

    Se arreglo el error de compilacion, ahora compila exitoso

--- robot-car-arduino.cpp
+++ robot-car-arduino.cpp
@@ -7,28 +7,25 @@ MICROPROGRAMACION 2020
 /* **************************************
 **      CLASE ULTRASONIDO
 ************************************** */
-//Constructor parametrisado
-Ultrasonido::Ultrasonido(byte *_pinTrigger,byte *_pinEcho){
-	pinTrigger=_pinTrigger;
-	pinEcho=_pinEcho;
-	pinMode(*pinTrigger, OUTPUT);
-	pinMode(*pinEcho, INPUT);
-}
-Ultrasonido::Ultrasonido(){}
 
-//hace la medicion de distancia devuelve cms de distancia hasta 350cm maxi
+Ultrasonido::Ultrasonido(byte *_pinTrigger, byte *_pinEcho){
+    pinTrigger=_pinTrigger;
+    pinEcho=_pinEcho; 
+    pinMode(*pinTrigger, OUTPUT);
+    pinMode(*pinEcho, INPUT);
+}
+Ultrasonido::Ultrasonido(){} 
+
 int Ultrasonido::medirCM(){
-    digitalWrite(*pinTrigger, LOW);
+    digitalWrite(*pinTrigger, LOW); 
     delayMicroseconds(4);
-    digitalWrite(*pinEcho, HIGH);
+    digitalWrite(*pinTrigger, HIGH);
     delayMicroseconds(10);
     digitalWrite(*pinTrigger, LOW);
-    distancia=pulseIn(*pinEcho, HIGH)/56.5812;
-    distancia=constrain(distancia,0,350);
-    return (int) distancia;
+    distancia = pulseIn(*pinEcho, HIGH)/56.5812;
+    distancia = constrain(distancia,0,350);
+    return (int) distancia; 
 }
-
-
 
 
 
@@ -41,109 +38,116 @@ int Ultrasonido::medirCM(){
 volatile unsigned int Carro::contaI=0;
 volatile unsigned int Carro::contaD=0;
 
-Carro::Carro(){
+ Carro::Carro(){
 	//configurando los pinMode del la potencia del carro
     //encoder, adelante, atras, potencia
-    pinMode(encoderI,INPUT);
-    pinMode(encoderD,INPUT);
-    pinMode(adelanteI,OUTPUT);
-    pinMode(atrasI,OUTPUT);
-    pinMode(potenciaI,OUTPUT);
-    pinMode(adelanteD,OUTPUT);
-    pinMode(atrasD,OUTPUT);
-    pinMode(potenciaD,OUTPUT);
+    pinMode(encoderI, INPUT);
+    pinMode(encoderD, INPUT);
+    pinMode(adelanteI, OUTPUT);
+    pinMode(atrasI, OUTPUT); 
+    pinMode(potenciaI, OUTPUT); 
+    pinMode(adelanteD, OUTPUT); 
+    pinMode(atrasD,OUTPUT); 
+    pinMode(potenciaD, OUTPUT);
 
     //instanciando los sensores ultrasonido y refiriendo
     // invoco constructor parametrizado, new devuelve un puntero
+    UltraC = Ultrasonido(&sensortrigerC, &sensorechoC);
+    UltraI = Ultrasonido(&sensortrigerI, &sensorechoI);
+    UltraD = Ultrasonido(&sensortrigerD, &sensorechoD);
+
+
     //UltraD->medir(); //para acceder a sus miembros uso -> por ser puntero
-    UltraC=Ultrasonido(&sensortrigerC,&sensorechoC);
-    UltraI=Ultrasonido(&sensortrigerI,&sensorechoI);
-    UltraD=Ultrasonido(&sensortrigerD,&sensorechoD);
+
     
 
 	//Agregamos este codigo para registrar las interrupciones
     //RISING indica que se disparara la interrupcion cuando el pin cambie de 0 a 1
-    attachInterrupt(digitalPinToInterrupt(encoderI), Carro::contarRuedaI,RISING);
-    attachInterrupt(digitalPinToInterrupt(encoderD), Carro::contarRuedaD,RISING);
-	Carro::contaI=0; //contadores a cero
-    Carro::contaD=0;
-}
+     attachInterrupt(digitalPinToInterrupt(encoderI), Carro::contarRuedaI,RISING);
+     attachInterrupt(digitalPinToInterrupt(encoderD), Carro::contarRuedaD,RISING);
+    	Carro::contaI=0; //contadores a cero
+        Carro::contaD=0;
+ }
 
 //METODOS QUE SE EJECUTAN CON LA INTERRUPCION
 //Son metodos Staticos pero static va en el .H
-void Carro::contarRuedaI(){
-    if(Carro::contaI<65000)Carro::contaI++; 
-    else Carro::contaI=1;    
-}
-void Carro::contarRuedaD(){
-    if(Carro::contaD<65000) Carro::contaD++;
-    else Carro::contaD=1;    
-}
+    void Carro::contarRuedaI(){
+        if(Carro::contaI<65000)Carro::contaI++; 
+        else Carro::contaI=1;    
+    }
+    void Carro::contarRuedaD(){
+        if(Carro::contaD<65000) Carro::contaD++;
+        else Carro::contaD=1;    
+    }
 
 //metodo para mover el carro + adelante, - atras, potencia abs
-void Carro::mover(){
-    izquierda = constrain(izquierda, -10, 10);
-    derecha = constrain(derecha, -10, 10);
+    void Carro::mover(int izquierda, int derecha){
+        izquierda = constrain(izquierda, -10, 10); 
+        derecha = constrain(derecha, -10, 10); 
 
-    //Rueda izquierda
-    if (izquierda<0){
-        digitalWrite(adelanteI,LOW);
-        digitalWrite(atrasI,HIGH);
-    }
-    else if(izquierda>0){
-        digitalWrite(adelanteI,HIGH);
-        digitalWrite(atrasI,LOW);
-    }else{
-        digitalWrite(adelanteI,LOW);
-        digitalWrite(atrasI,LOW);
-    }
-
-    //Rueda derecha
-    if (derecha<0){
+        //rueda izquierda
+        if(izquierda<0){
+            digitalWrite(adelanteI, LOW); 
+            digitalWrite(atrasI, HIGH);
+        }
+        else if(izquierda>0){
+            digitalWrite(adelanteI, HIGH); 
+            digitalWrite(atrasI, LOW);  
+        }
+        else
+        {
+            digitalWrite(adelanteI, LOW);
+            digitalWrite(atrasD, LOW);
+        }
+        //Rueda Derecha
+        if (derecha<0){
         digitalWrite(adelanteD,LOW);
         digitalWrite(atrasD,HIGH);
-    }
-    else if(derecha>0){
+        }
+        else if(derecha>0){
         digitalWrite(adelanteD,HIGH);
         digitalWrite(atrasD,LOW);
-    }else{
+        }else{
         digitalWrite(adelanteD,LOW);
         digitalWrite(atrasD,LOW);
+        }
+        
+        
+        //Potencia indicada en pines PWM
+        //AJUSTANDO LA POTENCIA
+        //Si tienes contadores de vuelta aqui agregaras el ajuste para que avance reto 
+        derecha=abs(derecha);
+        izquierda=abs(izquierda);
+
+        izquierda= map(izquierda,0,10,0,potenciaMAX);
+        derecha=map(derecha,0,10,0,potenciaMAX);
+        analogWrite(potenciaD,derecha);
+        analogWrite(potenciaI,izquierda); 
+        
+        
     }
-
-    derecha = abs(derecha);
-    izquierda = abs(izquierda);
-
-    izquierda = map(izquierda,0,10,0,potenciaMAX);
-    derecha = map(derecha,0,10,0,potenciaMAX);
-    analogWrite(potenciaD,derecha);
-    analogWrite(potenciaI,izquierda);
-
-}
 
     //Gira la cantidad de grados especifica: - a la izquierda, + derecha
 	//usa los contadores de vuelta para la precision al girar
 	//tu decides si usas dos llantas para girar o solo una
     //Si tienes contadores de rueda los usaras para hacer el giro
     //si no tienes contadores haras un aproximado con delay(tiempo)
-void Carro::girar(int grados){
-    float medioGiro=40;
-    int parar = grados*(medioGiro/180);
-    parar = abs(parar);
-    mover(0,0);
-    delay(100);
-    contaI=0;
-    contaD=0;
-    if(grados>0){
-        mover(10,-10);//girando moviendo las dos llantas
-    }else if(grados<0){
-        mover(-10,10);
+    void Carro::girar(int grados){
+        float medioGiro=40;
+        int parar=grados*(medioGiro/180);
+        parar=abs(parar);
+        mover(0,0);
+        delay(100);
+        contaI=0;
+        contaD=0;
+        if(grados>0){
+            mover(10,-10); //el giro(giro a la derecha) se realiza moviendo ambas llantas una hacia adelante y la otra hacia atras
+        }else if(grados<0){
+            mover(-10,10);//giro a la izquierda
+        }
+        while(contaI + contaD < parar){
+
+        }
+        mover(0,0);
+        delay(100);
     }
-    while(contaI + contaD < parar){
-
-    }
-    mover(0,0);
-    delay(100);
-}
-
-
--- robot-car-arduino.h
+++ robot-car-arduino.h
@@ -24,22 +24,20 @@ ciclo I-2019   DEFINA LAS SIGUIENTES CLASES
 ****************************/
 
 class Ultrasonido {
-	
-	private:
+	private: 
 		long distancia;
-	private:
+	public:
 		byte *pinEcho;
-		byte *pinTrigger;
+		byte  *pinTrigger;
 
-	//Constructor parametrizado Recibe como parametros trigger y Echo como byte
-	Ultrasonido(byte *_pinTrigger,byte *_pinEcho);
-	Ultrasonido();
+//Contructor parametrizado Recibe como parametros Trigger y Echo como byte 
+Ultrasonido(byte *_pinTrigger, byte *_pinEcho);
+Ultrasonido(); 
 
-	//Mide la distancia el sensor es preciso hasta 5m
-	//por tanto el valor maximo son 350 cm
-	int medirCM();
+//mide la distancia el sensor es preciso hasta 3.5mts 
+//por lo tanto el valor maximo es 350 cms
+int medirCM(); 
 };
-
 
 
 
@@ -90,49 +88,50 @@ class Ultrasonido {
 *******************************************************************************/
 
 class Carro{
+	public: 
+	byte encoderI=2; //pin del encoder de la llanta izquierda
+	byte encoderD=3; //pin del encoder de la llanta derecha
+	byte adelanteI=4;//pin avance adelante llanta izquierda
+	byte atrasI=5;	// pin de reversa llanda izquierda
+	byte potenciaI=6;//pin de potencia llanta izquierda pin PWM
+	byte adelanteD=7;//Adelante llanta derecha 
+	byte atrasD=8; // Atras llanta Derecha 
+	byte potenciaD=9; //potencia llanta Derecha pin PWM
+	byte sensortrigerC=10; //Sensor Ultrasonido Central Trigger
+	byte sensorechoC=11; //Sensor Ultrasonido Central Echo
+	byte sensortrigerI=12; //Sensor Ultrasonido Izquierda Trigger
+	byte sensorechoI=13; //Sensor Ultrasonido Izquierda Echo
+	byte sensortrigerD=14; //Sensor Ultrasonido Derecha Trigger
+	byte sensorechoD=15; //Sensor Ultrasonido Derecha Echo
+	byte potenciaMAX=200;
 
-		public:
-		byte encoderI = 2; //pin del encoder de la llanta izquierda
-		byte encoderD = 3; // pin del encoder de la llanta derecha
-		byte adelanteI =4; // pin avance adelante llanta Izquierda
-		byte atrasI =5;    // pin de reversa llanta izquierda
-		byte potenciaI =6; // pin de potencia llanta Izquierda pin PWM
-		byte adelanteD =7; // Adelante llanta Derecha
-		byte atrasD =8;  // atras llanta Derecha
-		byte potenciaD =9;  // potencia llanta Derecha pin PWM
-		byte sensortrigerC =10; //Sensor Ultrasonido Central Trigger
-		byte sensorechoC =11; //Sensor Ultrasonido Central Echo
-		byte sensortrigerI =12; //Sensor Ultrasonido Izquierda Trigger
-		byte sensorechoI =13; //Sensor Ultrasonido Izquierda Echo
-		byte sensortrigerD =14; //Sensor Ultrasonido Derecha Trigger
-		byte sensorechoD =15; //Sensor Ultrasonido Derecha Echo
-		byte potenciaMAX =200;  //limite regulador de la potencia maxima
 
-		Ultrasonido UltraC;
-		Ultrasonido UltraI;
-		Ultrasonido UltraD;
+	Ultrasonido UltraC;
+	Ultrasonido UltraI; //en el cpp se va instanciar y a referir 
+	Ultrasonido UltraD;
 
-		//DEFINIENDO LAS INTERRUPCIONES--USAN MIEMBROS ESTATICOS
-		static volatile unsigned int contaI; //contador de las vueltas de la llanta Izquierda
-  		static volatile unsigned int contaD; //contador de las vueltas de la llanta Derecha
+	//DEFINIENDO LAS INTERRUPCIONES -- USAN MIEMBROS ESTATICOS 
+	static volatile unsigned int contaI; // contador de vueltas de la llanta izquierda
+	static volatile unsigned int contaD; // contador de vueltas de la llanta derecha
+	//CONTADORES DE RUEAS IZQUIERDA Y DERECHA
+	static void contarRuedaI(); //metodo a registrar con la interrupcion en constructor
+	static void contarRuedaD(); 
 
-		//METODOS CONTADORES DE RUEDAS IZQUIERDA Y DERECHA
-		static void contarRuedaI(); //metodo a registrar con la interrupcion en constructor
-		static void contarRuedaD();
+	//Contructor -- configura variables, sensores e interrupciones
+	Carro(); 
 
-		Carro();
+	//acciona el motor, recibe como parametro la accion en la llanta izquierda y la llanta derecha 
+	//el valor de cada llanta si es positivo movira la llanta hacie adelante 
+	//si es negativo movera la llanta hacia atras 
+	//el numero representa la potencia aplicada a cada llanta 
+	//los valores pueden ir desde -255 hasta 255
+	void mover(int Izquierda, int Derecha);
 
-		//acciona el motor, recibe como parametro la accion en la llanta izquierda y la llanta derecha
-	// el valor de cada llanta si es positivo movera la llanta hacia adelante
-	// si es negativo movera la llanta hacia atras
-	// el numero representara la potencia aplicada a cada llanta
-	// los valores pueden ir de -10 hasta 10
-		void mover(int Izquierda, int Derecha);
+	//gira la cantidad de grados especifica: - a la izquierda, + derecha
+	//usa los contadores de vuelta para la proteccion al girar 
+	//tu decides si usas dos llantas para girar o solo una 
+	void girar(int grados);
 
-		//Gira la cantidad de grados especifica: - a la izquierda, + derecha
-	//usa los contadores de vuelta para la precision al girar
-	//tu decides si usas dos llantas para girar o solo una
-		void girar(int grados);
 };
 
 #endif
