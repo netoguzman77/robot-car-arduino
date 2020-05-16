a595da9b25cd740b7337c000d950416a3443906b
jmms27 <yekita2703@gmail.com>
2020-05-16 09:46:43 -0600

    Create AutomataCarro.ino (MS12036)
    Modificando algunos archivos de AutomataCarro

--- AutomataCarro/AutomataCarro.ino
+++ AutomataCarro/AutomataCarro.ino
@@ -5,11 +5,11 @@ int giro=-90;
  
 void setup(){
 //configuraciones del carro
-carrito.potenciaMAX=150;//definimos la velocidad con la que se movera el carro
+carrito.potenciaMAX=150; //definimos la velocidad con la que se movera el carro
 }
 void loop(){
 //si la lectura del sensor central es mayor a los cms para detener pues avanzamos
-if(carrito.UltraC.medirCM()>detener){
+if(carrito.UltraC.medirCM()> detener){
 //movemos el carrito hacia adelante
 carrito.mover(10,10);
 
@@ -18,7 +18,7 @@ carrito.mover(10,10);
 carrito.mover(0,0);
 delay(100);
 //evaluo para que lado esta despejado con los sensores laterales
-if(carrito.UltraI.medirCM()>carrito.UltraD.medirCM()){
+if(carrito.UltraI.medirCM() > carrito.UltraD.medirCM()){
 //si tengo la abertura a la izquierda
 carrito.girar(giro);
 //si no tengo tengo contadores de vuelta
@@ -30,5 +30,6 @@ carrito.girar(-1*giro);
 //si no tengo contadores de vuelta
 //carrito.mover(10,-10);
 //delay(1000);
+}
 }
 }
